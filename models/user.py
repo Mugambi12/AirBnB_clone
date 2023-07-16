0fcbe2386deb751a451798f5115a92b262898b54
Gabriel Muthenya <gabriel@bildit.co>
2023-07-16 23:55:15 +0300

    User

--- models/user.py
+++ models/user.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
+"""Class model of a User."""
+
 
 class User(BaseModel):
-    def __init__(self, *args, **kwargs):
-        """Initialize User instance"""
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
-
-    def __str__(self):
-        """Return a string representation of the User instance"""
-        return "[User] ({}) {}".format(self.id, self.__dict__)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
