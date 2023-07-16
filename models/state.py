0032e18d850b784aa9c2c5f7969c4283d61c7638
Gabriel Muthenya <gabriel@bildit.co>
2023-07-16 23:57:19 +0300

    Update model

--- models/state.py
+++ models/state.py
@@ -3,11 +3,4 @@ from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    def __init__(self, *args, **kwargs):
-        """Initialize State instance"""
-        super().__init__(*args, **kwargs)
-        self.name = ""
-
-    def __str__(self):
-        """Return a string representation of the State instance"""
-        return "[State] ({}) {}".format(self.id, self.__dict__)
+    name = ""
